ffbd3afabf614c205bb6aa11ac575cf17a9cb2a9
StepanTsymbal <tsymbalstepan@gmail.com>
2016-07-02 09:33:37 +0300

    upd 1.1

--- src/main/java/com/styopik/controllers/ContactsController.java
+++ src/main/java/com/styopik/controllers/ContactsController.java
@@ -6,7 +6,11 @@ import java.util.concurrent.Callable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.ws.rs.core.MediaType;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,15 +27,20 @@ public class ContactsController {
 	@Autowired
 	ContactsRepository contactsRepository;
 
-	@RequestMapping(value = "/contacts", method = RequestMethod.GET)
-	public WebAsyncTask<List<Contacts>> handleTestRequest(@RequestParam("nameFilter") String nameFilter) {
+	@RequestMapping(value = "/contacts", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
+	// public WebAsyncTask<List<Contacts>>
+	// handleTestRequest(@RequestParam("nameFilter") String nameFilter) {
+	public WebAsyncTask<ResponseEntity<List<Contacts>>> handleTestRequest(
+			@RequestParam("nameFilter") String nameFilter) {
 
 		List<Contacts> filteredContacts = new ArrayList<Contacts>();
 
-		Callable<List<Contacts>> callable = new Callable<List<Contacts>>() {
+		// Callable<List<Contacts>> callable = new Callable<List<Contacts>>() {
+		Callable<ResponseEntity<List<Contacts>>> callable = new Callable<ResponseEntity<List<Contacts>>>() {
 
 			@Override
-			public List<Contacts> call() throws InterruptedException {
+			// public List<Contacts> call() throws InterruptedException {
+			public ResponseEntity<List<Contacts>> call() throws InterruptedException {
 
 				for (Contacts contact : contactsRepository.findAll()) {
 					Pattern pattern = Pattern.compile(nameFilter);
@@ -42,11 +51,16 @@ public class ContactsController {
 					}
 				}
 
-				return filteredContacts;
+				if (filteredContacts.isEmpty()) {
+					return new ResponseEntity<List<Contacts>>(filteredContacts, HttpStatus.NOT_FOUND);
+				}
 
+				// return filteredContacts;
+				return new ResponseEntity<List<Contacts>>(filteredContacts, HttpStatus.OK);
 			}
 		};
 
-		return new WebAsyncTask<List<Contacts>>(callable);
+		// return new WebAsyncTask<List<Contacts>>(callable);
+		return new WebAsyncTask<ResponseEntity<List<Contacts>>>(callable);
 	}
 }
